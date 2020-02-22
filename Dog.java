69df6375e412730c6f2fc95c06a90334f2c611fa
ELPSYCONGROO <huangyn38@mail2.sysu.edu.cn>
2020-02-22 18:57:20 +0800

    updated in 2020/2/22

---
+++ Dog.java
@@ -0,0 +1,7 @@
+public class Dog{
+    public int weight;
+    public static String name="happy";
+    public Dog(int w){
+        this.weight=w;
+    }
+}
---
+++ HelloWorld.java
@@ -0,0 +1,6 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        Dog [] dogs=new Dog[]{new Dog(5),new Dog(15)};
+        System.out.println(dogs[0].weight);
+    }
+}
