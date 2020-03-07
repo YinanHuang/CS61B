1783f9e06c2fbe1a729cbe6b55e2330f40639884
ELPSYCONGROO <huangyn38@mail2.sysu.edu.cn>
2020-03-08 00:17:24 +0800

    updated on 2020/3/8

---
+++ my/midTerm2/Map61B/ListNode.java
@@ -0,0 +1,9 @@
+package Map61B;
+
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) {
+        this.val = x;
+    }
+}
--- my/midTerm2/Map61B/Test.java
+++ my/midTerm2/Map61B/Test.java
@@ -1,28 +1,67 @@
 package Map61B;
 import java.util.*;
-import java.util.Iterator;
 
-public class Test {
-    public static void main(String[] args){
-        ArraySet<Integer> set = new ArraySet<Integer>();
-        set.add(1);
-        set.add(2);
-        Iterator<Integer> it = set.iterator();
-        System.out.println(set.toString());
-        ArrayList<Integer> list;
-    }
-    public static boolean twoSum(int[] A, int x){
-        int left = 0;
-        int right = A.length;
-        while(left != right){
-            if(A[left] + A[right] == x){
-                return true;
-            } else if(A[left] + A[right] < x){
-                left++;
+
+class Solution2 {
+    public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode sum = new ListNode(-1);
+        ListNode ans = sum;
+        int temp;
+        int flag = 0;
+        while(l1 != null && l2 != null){
+            temp = l1.val + l2.val + flag;
+            if(temp >= 10){
+                temp -= 10;
+                sum.next = new ListNode(temp);
+                flag = 1;
             } else{
-                right--;
+                sum.next = new ListNode(temp);
+                flag = 0;
+            }
+            sum = sum.next;
+            l1 = l1.next;
+            l2 = l2.next;
+        }
+        ListNode left = helper(l1, l2);
+        if(left != null){
+            while(left != null) {
+                temp = left.val + flag;
+                if(temp >= 10){
+                    temp -= 10;
+                    flag = 1;
+                    sum.next = new ListNode(temp);
+                } else {
+                    sum.next = new ListNode(left.val + flag);
+                    flag = 0;
+
+                }
+                sum = sum.next;
+                left = left.next;
             }
         }
-        return false;
+        if(flag == 1){
+            sum.next = new ListNode(1);
+        }
+        return ans.next;
+    }
+
+    private static ListNode helper(ListNode l1, ListNode l2){
+        if(l1 != null){
+            return l1;
+        } else{
+            return l2;
+        }
+    }
+
+    public static void main(String[] args){
+        ListNode l1 = new ListNode(3);
+        l1.next = new ListNode(7);
+        ListNode l2 = new ListNode(9);
+        l2.next = new ListNode(2);
+        ListNode ans = addTwoNumbers(l1, l2);
+        while(ans != null) {
+            System.out.println(ans.val);
+            ans = ans.next;
+        }
     }
 }
